6c068244dae0d8cc0299072f4fe2d6b107f78532
yağmur özkan <yagmurozkan812@gmail.com>
2026-03-28 16:19:15 +0300

    commit

--- OrbinatAPI/Program.cs
+++ OrbinatAPI/Program.cs
@@ -1,4 +1,12 @@
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 // Add services to the container.
 
@@ -7,7 +15,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
